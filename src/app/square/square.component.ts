b790b9c308900f13f50b0955a229c7b5739389ea
Jillian Aubrey <jillian.nk.aubrey@gmail.com>
2023-01-28 00:14:30 -0700

    Improve button styling

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -4,7 +4,8 @@ import { Component, Input } from '@angular/core';
   selector: 'app-square',
   template: `
     <button>{{ value }}</button>
-  `
+  `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
 
